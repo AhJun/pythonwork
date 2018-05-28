519cb741d8876bd4cee1f75643419a78c63a63b3
AhJun <37072604+AhJun@users.noreply.github.com>
2018-05-28 18:41:06 +0800

    Update ProduceMyDict.py

--- ProduceMyDict.py
+++ ProduceMyDict.py
@@ -1,80 +1,56 @@
-import re
+import os
 
-strs = open(r'C:/Users/myl/Desktop/SegChineseToWords/英汉词典TXT格式.txt','r',encoding='utf-8').readlines()
+def userOperateInterface():
+    print("\n请选择词典功能")
+    print("i: 添加单词")
+    print("s: 查询单词")
+    print("Q: 退出词典")
+    print("请选择功能：")
+    return input()
 
-for str in strs:
+def addWord(wordDict:dict, fileName):
+    str = input("您输入要加入的单词：")
+    if str in wordDict.keys():
+        print("该单词已添加到字典库\n")
+        userOperateInterface()
+    else:
+        t = input("请输入此单词的中文释义：")
+        wordDict[str] = t
+        with open(fileName, 'a') as fw:
+            fw.write(str + "  " + t + '\n')
 
-# 形容词典
-    adj_re = re.search('adj', str)
-    if adj_re != None:
-        adj_num = adj_re.end()+1
-        adj_str = str[adj_num:]
-        adj_list = re.findall("[\u4e00-\u9fa5]+", adj_str)
-        for ele_adj in adj_list:
-            ele_adj = ele_adj + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/adj_dict.txt', 'a+',encoding='utf-8') as f:
-                f.write(ele_adj)
-
-
-# 副词典
-    adv_re = re.search('adv', str)
-    if adv_re != None:
-        adv_num = adv_re.end()+1
-        adv_str = str[adv_num:]
-        adv_list = re.findall("[\u4e00-\u9fa5]+", adv_str)
-        for ele_adv in adv_list:
-            ele_adv = ele_adv + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/adv_dict.txt', 'a+',encoding='utf-8') as f:
-                f.write(ele_adv)
-
-# 名词词典
-    n_re = re.search('n', str)
-    if n_re != None:
-        n_num = n_re.end() + 1
-        n_str = str[n_num:]
-        n_list = re.findall("[\u4e00-\u9fa5]+", n_str)
-        for ele_n in n_list:
-            ele_n = ele_n + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/n_dict.txt', 'a+', encoding='utf-8') as f:
-                f.write(ele_n)
+def selectWord(wordDict:dict):
+    str = input("请输入您要查询的单词：")
+    if str not in wordDict.keys():
+        print("字典库中未找到这个单词\n")
+    else:
+        print(wordDict[str])
 
 
-# 动词词典
-    v_re = re.search('v', str)
-    if v_re != None:
-        v_num = v_re.end() + 1
-        v_str = str[v_num:]
-        v_list = re.findall("[\u4e00-\u9fa5]+", v_str)
-        for ele_v in v_list:
-            ele_v = ele_v + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/v_dict.txt', 'a+', encoding='utf-8') as f:
-                f.write(ele_v)
+def main():
+    wordDict = {}
+    if os.path.exists("dict.txt"):
+        with open("dict.txt", 'r') as fr:
+            for ln in fr:
+                s = ln.split(" ")
+                wordDict[s[0]] = s[1]
+    else:
+        fw = open("dict.txt",'w')
+        fw.close()
+    print("******欢迎使用简明英汉词典******")
+    while True:
+        op = userOperateInterface()
+        if op == 'i':
+            addWord(wordDict, 'dict.txt')
+        elif op == 's':
+            selectWord(wordDict)
+        elif op == 'Q':
+            break
+        else:
+            print("输入有误\n")
 
 
-# 介词词典
-    prep_re = re.search('prep', str)
-    if prep_re != None:
-        prep_num = prep_re.end() + 1
-        prep_str = str[prep_num:]
-        prep_list = re.findall("[\u4e00-\u9fa5]+", prep_str)
-        for ele_prep in prep_list:
-            ele_prep = ele_prep + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/prep_dict.txt', 'a+', encoding='utf-8') as f:
-                f.write(ele_prep)
-
-# 代词词典
-    pron_re = re.search('pron', str)
-    if pron_re != None:
-        pron_num = pron_re.end() + 1
-        pron_str = str[pron_num:]
-        pron_list = re.findall("[\u4e00-\u9fa5]+", pron_str)
-        for ele_pron in pron_list:
-            ele_pron = ele_pron + '\n'
-            with open(r'C:/Users/myl/Desktop/SegChineseToWords/Dict/pron_dict.txt', 'a+', encoding='utf-8') as f:
-                f.write(ele_pron)
-
-
-
+main()
 
 
 
